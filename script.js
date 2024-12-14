7f6a0be72d717c03165bd55333ac7618a9e070dd
Ayush Jaiswal <ayyushjaiswal@gmail.com>
2024-12-14 21:00:09 +0530

    Simplified and revamped portfolio website

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-// Smooth Scroll for Navigation Links
-document.querySelectorAll('.nav-links a').forEach(link => {
-    link.addEventListener('click', function (e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        target.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    });
-});
+console.log("Welcome to Ayush's website!");
 
-// Console Greeting
-console.log("Welcome to Ayush's Personal Website! Let's connect and collaborate!");
