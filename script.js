a7051462b6189557d807a695c2d59092999b4a19
Ayush Jaiswal <ayyushjaiswal@gmail.com>
2024-12-14 21:13:21 +0530

    Simplified and revamped portfolio website

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-console.log("Welcome to Ayush's website!");
+const words = ['Designer', 'Developer', 'Creator', 'Innovator'];
+let currentIndex = 0;
 
+function changeText() {
+    const changingText = document.getElementById('changing-text');
+    changingText.textContent = words[currentIndex];
+    currentIndex = (currentIndex + 1) % words.length;
+}
+
+// Change text every 2 seconds
+setInterval(changeText, 2000);
